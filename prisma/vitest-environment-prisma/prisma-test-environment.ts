da6a6094dc120843aa99d21e8c21152250a561fe
maycow jordny <maycowjordny2015@gmail.com>
2023-12-22 10:18:37 -0300

    atualizando arqivos de config

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
+
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
 import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
-import { execSync } from "node:child_process";
-import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -34,6 +35,7 @@ export default <Environment>{
         await prisma.$executeRawUnsafe(
           `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
         );
+
         await prisma.$disconnect();
       },
     };
